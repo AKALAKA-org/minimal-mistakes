a1727ee8fea0f25e8444398cd011709695bf1031
SameerRau <sameer.rau2@gmail.com>
2022-04-27 12:45:32 -0400

    Update to handle email gateway

--- form-handler.php
+++ form-handler.php
@@ -3,4 +3,19 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
+
+$email_from = 'hi@akalaka.org'
+$email_subject = 'Subject Entry'
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'hi@akalaka.org';
+
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+header("Location: contact.html");
 ?>
