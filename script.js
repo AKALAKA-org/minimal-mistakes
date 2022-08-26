27c8e2bd13a3ab3dbab822dd56ce6a4771d700cc
SameerRau <sameer.rau2@gmail.com>
2022-08-26 12:53:53 -0400

    Update with new js login/registration click actions

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 const container = document.querySelector(".container");
+const pwShowHide = document.querySelectorAll(".showHidePw");
+const pwFields = document.querySelectorAll(".password");
+const signUp = document.querySelector(".signup-link");
+const login = document.querySelector(".login-link");
 const signUpBtn = document.querySelector(".color-bg button");
 
-signUpBtn.addEventListener('click', () => {
-  container.classList.toggle('change');
+pwShowHide.forEach(eyeIcon => {
+  eyeIcon.addEventListener("click", () => {
+    pwFields.forEach(pwField => {
+      if(pwField.type === "password") {
+        pwField.type = "text";
+        
+        pwShowHide.forEach(icon => {
+          icon.classList.replace("uil-eye-slash", "uil-eye");
+        });
+      } else {
+        pwField.type = "password";
+        
+        pwShowHide.forEach(icon => {
+          icon.classList.replace("uil-eye", "uil-eye-slash");
+        });
+      }
+    });
+  });
 });
+
+signUp.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+login.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+//signUpBtn.addEventListener("click", () => {
+//  container.classList.toggle("change");
+//});
